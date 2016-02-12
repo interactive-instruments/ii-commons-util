e3b04f22e526e89f50515d4f52cef7fd1fb9df29
Jon Herrmann <herrmann@interactive-instruments.de>
2016-02-12 03:24:16 +0100

    io utilities

---
+++ src/main/java/de/interactive_instruments/io/DirSizeVisitor.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2016 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.interactive_instruments.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * Calculate recursively the size of all (filtered) files in a directory.
+ *
+ * @author J. Herrmann ( herrmann <aT) interactive-instruments (doT> de )
+ */
+public class DirSizeVisitor implements FileVisitor<Path> {
+
+    private long size=0;
+    private final PathFilter filter;
+
+    /**
+     * Calculate size of filtered files in the directory
+     * @param filter A PathFilter or null
+     * @param filter PathFilter
+     */
+    public DirSizeVisitor(PathFilter filter) {
+        if(filter==null) {
+            this.filter = (p) -> true;
+        }else {
+            this.filter = filter;
+        }
+    }
+
+    /**
+     * Get calculated size
+     * @return
+     */
+    public long getSize() {
+        return size;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if(filter.accept(file)) {
+            size += attrs.size();
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+}
---
+++ src/main/java/de/interactive_instruments/io/FileChangeListener.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2016 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.interactive_instruments.io;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.util.*;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.stream.Collectors;
+
+/**
+ * @author herrmann@interactive-instruments.de.
+ */
+@FunctionalInterface
+public interface FileChangeListener {
+
+    /**
+     * Default fileChanged() delegates the call to the filesChanged() method.
+     *
+     * @param watchableEventMap
+     */
+    default void fileChanged(final Map<Path, List<WatchEvent<?>>> watchableEventMap) {
+        final Set<Path> dirSet = new TreeSet<>();
+        final Map<Path, WatchEvent.Kind> pathEventMap = new TreeMap<>();
+        watchableEventMap.entrySet().forEach( wE ->
+                wE.getValue().forEach(event -> {
+                    final Path path = wE.getKey().resolve((Path) event.context());
+                    if(Files.isDirectory(path)) {
+                        dirSet.add(path);
+                    }else{
+                        dirSet.add(path.getParent());
+                    }
+                    pathEventMap.put(path, event.kind());
+        }));
+        filesChanged(pathEventMap, dirSet);
+    }
+
+
+    /**
+     * Informs the observer that files have changed.
+     *
+     * @param eventMap a map containing all full file paths that changed and the corresponding event kinds.
+     * @param dirs a set of the directories that changed or in which files changed.
+     */
+    void filesChanged(final Map<Path, WatchEvent.Kind> eventMap, final Set<Path> dirs);
+}
