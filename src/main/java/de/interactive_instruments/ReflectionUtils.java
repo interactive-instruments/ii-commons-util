fdc7a3c34db522de3a88452251dd4d9324f0eef3
Jon Herrmann <herrmann@interactive-instruments.de>
2017-07-06 12:17:50 +0200

    setObjectField

--- src/main/java/de/interactive_instruments/ReflectionUtils.java
+++ src/main/java/de/interactive_instruments/ReflectionUtils.java
@@ -65,4 +65,22 @@ public class ReflectionUtils {
 			return false;
 		}
 	}
+
+
+	public static boolean setObjectField(final Object object, final String fieldName, final Object fieldValue) {
+		Class<?> clazz = object.getClass();
+		while (clazz != null) {
+			try {
+				final Field field = clazz.getDeclaredField(fieldName);
+				field.setAccessible(true);
+				field.set(object, fieldValue);
+				return true;
+			} catch (NoSuchFieldException e) {
+				clazz = clazz.getSuperclass();
+			} catch (Exception e) {
+				throw new IllegalStateException(e);
+			}
+		}
+		return false;
+	}
 }
