68766cefcde118e30f6248fcb77678804f1807e8
Jon Herrmann <herrmann@interactive-instruments.de>
2018-09-19 11:48:05 +0200

    ListReverseView

---
+++ src/main/java/de/interactive_instruments/collections/ListReverseView.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2017-2018 European Union, interactive instruments GmbH
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ * This work was supported by the EU Interoperability Solutions for
+ * European Public Administrations Programme (http://ec.europa.eu/isa)
+ * through Action 1.17: A Reusable INSPIRE Reference Platform (ARE3NA).
+ */
+package de.interactive_instruments.collections;
+
+import java.util.AbstractList;
+import java.util.List;
+
+/**
+ * Reverse wrapper view on a List object.
+ *
+ * Note: the wrapper does not create a copy of the original list!
+ *
+ * @author Jon Herrmann ( herrmann aT interactive-instruments doT de )
+ */
+public class ListReverseView<T> extends AbstractList<T> {
+
+	private final List<T> wrappedList;
+
+	public ListReverseView(final List<T> wrappedList) {
+		this.wrappedList = wrappedList;
+	}
+
+	@Override
+	public T get(int i) {
+		return wrappedList.get(wrappedList.size() - i - 1);
+	}
+
+	@Override
+	public int size() {
+		return wrappedList.size();
+	}
+
+	public List<T> normalView() {
+		return wrappedList;
+	}
+}
